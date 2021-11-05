62346e8ea65c5eb56fb6d871b985710d04114125
Umair Asghar <56846748+UmairMughal901@users.noreply.github.com>
2021-11-05 16:42:23 +0500

    front end level 2

--- src/Components/Temp.js
+++ src/Components/Temp.js
@@ -1,25 +1,91 @@
-import React from 'react';
-import "../CSS/ThapaWeather.css"
+import React from "react";
+import "../CSS/ThapaWeather.css";
 
 const Temp = () => {
-    return (
-        <>
-        <div className="wrap">
-            <div className="search">
-                <input type="search" 
-                type="search"
-                placeholder="Search..."
-                autoFocus
-                id="search"
-                className="searchTerm"
-                />
-                <button className="searchButton" type="button">
-                    Search
-                </button>
-            </div>
+  return (
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            type="search"
+            placeholder="Search..."
+            autoFocus
+            id="search"
+            className="searchTerm"
+          />
+          <button className="searchButton" type="button">
+            Search
+          </button>
         </div>
-        </>
-    )
-}
+      </div>
+
+      {/* Main Card for weather information */}
+      <article className="widget">
+        <div className="weatherIcon">
+          <i className={"wi wi-day-sunny"}></i>
+        </div>
+        <div className="weatherInfo">
+          <div className="temperature">
+            <span>25.5&deg;</span>
+          </div>
+          <div className="description">
+            <div className="weatherCondition">Sunny</div>
+            <div className="place">Lahore, Pakistan</div>
+          </div>
+        </div>
+        <div className="date">{new Date().toLocaleString()}</div>
+
+        {/* our 4 divided section */}
+        <div className="extra-temp">
+          <div className="temp-info-minmax">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-sunset"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:18 PM
+                <br />
+                Sunset
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-humidity"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:18 PM
+                <br />
+                Humidity
+              </p>
+            </div>
+          </div>
+          <div className="weather-extra-info">
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-rain"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:18 PM
+                <br />
+                Rain
+              </p>
+            </div>
+            <div className="two-sided-section">
+              <p>
+                <i className={"wi wi-strong-wind"}></i>
+              </p>
+              <p className="extra-info-leftside">
+                19:18 PM
+                <br />
+                Speed
+              </p>
+            </div>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+};
 
 export default Temp;
